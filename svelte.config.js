71ee9fd694cef30ae05024913642b8e66792b271
loganynguyen <logan.y.nguyen@gmail.com>
2021-12-17 06:07:30 -0500

    Tauri now runs

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
+		ssr: false,
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
