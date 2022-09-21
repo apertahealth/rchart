1444df6129d393ac1ec895575e731afd57380679
loganynguyen <logan.y.nguyen@gmail.com>
2022-09-21 02:23:30 -0400

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,15 @@
   windows_subsystem = "windows"
 )]
 
+use std::sync::{Arc, Mutex};
+use Tauri::State;
+
+#[derive(Default)]
+struct Counter(Arc<Mutex<i32>>);
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![])
+    .manage(Counter(Default::default()))
+    .invoke_handler(tauri::generate_handler![hello_world, add_count])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -13,4 +19,12 @@ fn main() {
 #[tauri::command]
 fn hello_world() -> String {
   "Hello World!".to_string()
+}
+
+#[tauri::command]
+fn add_count(num: i32, counter: State<'_, Counter>) -> String {
+  let mut val = counter.0.lock().unwrap();
+  *val += num;
+
+  format!("{val}")
 }
