7c6e38a5b98d500d175ad2e474dbf9f3e0e1183d
loganynguyen <logan.y.nguyen@gmail.com>
2022-10-05 23:32:35 -0400

    added status bar, simplified divs in the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,129 +30,125 @@
 </section>
 
 <!-- side bar -->
-<section>
-	<div
-		class="fixed top-0 left-0 h-screen w-20 m-0 flex flex-col bg-gray-50
-				text-white shadow-lg"
-	>
-		<!-- the sidebar content-->
-		<div class="mt-3">
-			<div class="group">
-				<div class="relative flex items-center justify-center">
-					<img
-						class="w-16 h-16 rounded-full border-2 border-blue-400"
-						src="./src/lib/img/doctor_headshot.jpg"
-						alt="profile-pic"
-					/>
-					<div>
-						<img
-							class="w-8 h-8 rounded-full justify-center absolute bottom-0 right-0"
-							src="./src/lib/img/wsu.png"
-							alt="institution"
-						/>
-						<span class="w-4 h-4 rounded-full bg-green-500 absolute bottom-0 left-2" />
-					</div>
-				</div>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-						shadow-md text-white bg-gray-600 text-xs font-bold
-						transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					profile
-				</span>
-			</div>
-
-			<div class="group">
-				<i
-					class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
-					mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
-					hover:text-white rounded-3xl hover:rounded-xl transition-all
-					duration-300 ease-liner"
-				>
-					<i class="fa-solid fa-calendar-days h-8 w-8" />
-				</i>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-							shadow-md text-white bg-gray-900 text-xs font-bold
-							transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					dashboard
-				</span>
-			</div>
-
-			<div class="group">
-				<i
-					class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
-					mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
-					hover:text-white rounded-3xl hover:rounded-xl transition-all
-					duration-300 ease-liner"
-				>
-					<i class="fa-solid fa-list h-8 w-8" />
-				</i>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-							shadow-md text-white bg-gray-900 text-xs font-bold
-							transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					Patient List
-				</span>
-			</div>
-
-			<div class="group">
-				<i
-					class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
-					mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
-					hover:text-white rounded-3xl hover:rounded-xl transition-all
-					duration-300 ease-liner"
-				>
-					<i class="fa-solid fa-people-group h-8 w-8" />
-				</i>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-							shadow-md text-white bg-gray-900 text-xs font-bold
-							transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					Team
-				</span>
-			</div>
-
-			<div class="group">
-				<i
-					class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
-					mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
-					hover:text-white rounded-3xl hover:rounded-xl transition-all
-					duration-300 ease-liner"
-				>
-					<i class="fa-solid fa-book-medical h-8 w-8" />
-				</i>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-							shadow-md text-white bg-gray-900 text-xs font-bold
-							transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					Resources
-				</span>
-			</div>
-
-			<div class="group">
-				<i
-					class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
-					mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
-					hover:text-white rounded-3xl hover:rounded-xl transition-all
-					duration-300 ease-liner"
-				>
-					<i class="fa-solid fa-puzzle-piece h-8 w-8 pl-1 pb-1" />
-				</i>
-				<span
-					class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
-							shadow-md text-white bg-gray-900 text-xs font-bold
-							transition-all duration-100 scale-0 origin-left group-hover:scale-100"
-				>
-					Extensions
-				</span>
+<section class="fixed top-0 left-0 h-screen w-20 m-0 flex flex-col bg-gray-50 text-white shadow-lg">
+	
+	<!-- profile button-->
+	<div class="group">
+		<div class="relative flex items-center justify-center mt-3">
+			<img
+				class="w-16 h-16 rounded-full border-2 border-blue-400"
+				src="./src/lib/img/doctor_headshot.jpg"
+				alt="profile-pic"
+			/>
+			<div>
+				<img
+					class="w-8 h-8 rounded-full justify-center absolute bottom-0 right-0"
+					src="./src/lib/img/wsu.png"
+					alt="institution"
+				/>
+				<span class="w-4 h-4 rounded-full bg-green-500 absolute bottom-0 left-2" />
 			</div>
 		</div>
+
+		<!-- tooltip -->
+		<span class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+				shadow-md text-white bg-gray-600 text-xs font-bold
+				transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+			profile
+		</span>
 	</div>
+
+	<!-- side buttons -->
+	<div class="group">
+		<i
+			class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
+			mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
+			hover:text-white rounded-3xl hover:rounded-xl transition-all
+			duration-300 ease-liner"
+		>
+			<i class="fa-solid fa-calendar-days h-8 w-8" />
+		</i>
+		<span
+			class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+					shadow-md text-white bg-gray-900 text-xs font-bold
+					transition-all duration-100 scale-0 origin-left group-hover:scale-100"
+		>
+			dashboard
+		</span>
+	</div>
+
+	<div class="group">
+		<i
+			class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
+			mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
+			hover:text-white rounded-3xl hover:rounded-xl transition-all
+			duration-300 ease-liner"
+		>
+			<i class="fa-solid fa-list h-8 w-8" />
+		</i>
+		<span
+			class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+					shadow-md text-white bg-gray-900 text-xs font-bold
+					transition-all duration-100 scale-0 origin-left group-hover:scale-100"
+		>
+			Patient List
+		</span>
+	</div>
+
+	<div class="group">
+		<i
+			class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
+			mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
+			hover:text-white rounded-3xl hover:rounded-xl transition-all
+			duration-300 ease-liner"
+		>
+			<i class="fa-solid fa-people-group h-8 w-8" />
+		</i>
+		<span
+			class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+					shadow-md text-white bg-gray-900 text-xs font-bold
+					transition-all duration-100 scale-0 origin-left group-hover:scale-100"
+		>
+			Team
+		</span>
+	</div>
+
+	<div class="group">
+		<i
+			class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
+			mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
+			hover:text-white rounded-3xl hover:rounded-xl transition-all
+			duration-300 ease-liner"
+		>
+			<i class="fa-solid fa-book-medical h-8 w-8" />
+		</i>
+		<span
+			class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+					shadow-md text-white bg-gray-900 text-xs font-bold
+					transition-all duration-100 scale-0 origin-left group-hover:scale-100"
+		>
+			Resources
+		</span>
+	</div>
+
+	<div class="group">
+		<i
+			class="relative flex items-center justify-center h-16 w-16 mt-2 mb-2
+			mx-auto shadow-lg bg-gray-200 text-gray-600 hover:bg-gray-700
+			hover:text-white rounded-3xl hover:rounded-xl transition-all
+			duration-300 ease-liner"
+		>
+			<i class="fa-solid fa-puzzle-piece h-8 w-8 pl-1 pb-1" />
+		</i>
+		<span
+			class="absolute w-auto p-2 m-2 min-2-max left-20 rounded-md
+					shadow-md text-white bg-gray-900 text-xs font-bold
+					transition-all duration-100 scale-0 origin-left group-hover:scale-100"
+		>
+			Extensions
+		</span>
+	</div>
+	
 </section>
 
 <!-- page contents -->
@@ -162,9 +158,7 @@
 
 <!-- status bar -->
 <footer>
-	<section>
-		<div class="fixed bottom-0 left-0 bg-blue-600" />
-	</section>
+	<section class="absolute bottom-0 left-0 h-6 w-screen bg-blue-600"></section>
 </footer>
 
 <style>
