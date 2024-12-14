63b57fa8b029e2d1eb6f90e23ac3503fe5e4862a
loganynguyen <logan.y.nguyen@gmail.com>
2024-12-13 22:26:18 -0500

    status bar offline and online modes

--- src/lib/components/StatusBar.svelte
+++ src/lib/components/StatusBar.svelte
@@ -1,18 +1,43 @@
-<div class="absolute bottom-0 left-0 h-6 w-screen bg-green-600">
-    <div class="flex justify-end text-xs py-1">
-        <p class="font-bold mr-10 text-green-100">Online</p>
-        <p class="mr-10">|</p>
+<script lang="ts">
+	import { ConnectionStatusStore, setConnectionStatus } from '../../stores/ConnectionStatusStore';
+	$: $ConnectionStatusStore;
+</script>
 
-        <p class="mr-1">Last Edited @</p>
-        <p class="font-bold mr-10">5:15:21 PM EST</p>
-        <p class="mr-10">|</p>
+<div
+	class={`absolute bottom-0 left-0 h-6 w-screen ${
+		$ConnectionStatusStore
+			? 'bg-gradient-to-r from-green-600 via-green-400 to-green-600 bg-[length:200%_100%] animate-shimmer'
+			: 'bg-gradient-to-r from-red-400 via-red-600 to-red-400 bg-[length:200%_100%] animate-shimmer'
+	}`}
+>
+	<div class="flex justify-between text-xs p-1 mx-1">
+		<!-- Left Section -->
+		<div class="flex items-center ml-5">
+			<!-- Connection Status -->
+			<p class={`font-bold ${$ConnectionStatusStore ? 'text-green-100' : 'text-red-100'}`}>
+				{$ConnectionStatusStore ? 'Online' : 'Disconnected'}
+			</p>
+			<p class="text-gray-300 mx-8">|</p>
 
-        <p class="mr-1">Connected to</p>
-        <p class="font-bold mr-10 text-green-200">Birchwood Health</p>
-        <p class="mr-10">|</p>
+			<!-- Conditionally Render Connected To -->
+			{#if $ConnectionStatusStore}
+				<p>Connected to</p>
+				<p class={`font-bold text-green-200 ml-1`}>Birchwood Health</p>
+				<p class="text-gray-300 mx-8">|</p>
+			{/if}
 
-        <p class="mr-1 ">Synced to</p>
-        <p class="font-bold text-green-200 mr-1">20/20</p>
-        <p class="mr-10">nodes</p>
-    </div>
+			<!-- Synced Nodes -->
+			<p>Synced to</p>
+			<p class={`mx-1 font-bold ${$ConnectionStatusStore ? 'text-green-200' : 'text-red-200'}`}>
+				{$ConnectionStatusStore ? '20/20' : '0/20'}
+			</p>
+			<p>nodes</p>
+		</div>
+
+		<!-- Right Section -->
+		<div class="flex items-center">
+			<p class="mr-1">Last Edited @</p>
+			<p class="font-bold text-white">5:15:21 PM EST</p>
+		</div>
+	</div>
 </div>
--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,133 +1,142 @@
 <script context="module" lang="ts">
-    export const prerender = true;
-    import TabList from '../../lib/components/TabList.svelte';
-    let comp;
+	export const prerender = true;
+	import TabList from '../../lib/components/TabList.svelte';
+	import { goto } from '$app/navigation';
+
+	let comp;
 	import { invoke } from '@tauri-apps/api/tauri';
 	import TabStore from '../../stores/TabStore.js';
 
-    let data = [
-    {
-        "number": "001",
-        "pic": "",
-        "first_name": "Logan",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee"
-    },{
-        "number": "001",
-        "pic": "",
-        "first_name": "Sean",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee"
-    },{
-        "number": "001",
-        "pic": "",
-        "first_name": "Evan",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee"
-    },
-    {
-        "number": "001",
-        "pic": "",
-        "first_name": "Don",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee"
-    },
-    {
-        "number": "001",
-        "pic": "",
-        "first_name": "Mark",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee",
-        "HealthScore": "2"
-    },
-    {
-        "number": "001",
-        "pic": "",
-        "first_name": "Maxine",
-        "last_name": "Nguyen",
-        "DOB": "9/15/98",
-        "Age": "24",
-        "Sex": "M",
-        "Gender": "M",
-        "Address": "222 Main St. Detroit",
-        "Phone Number": "555-555-5555",
-        "Email": "lg@asd.com",
-        "Last Seen": "5/5/23",
-        "PCP": "Dr. Geedee",
-        "HealthScore": "2"
-    }
-    ];
+	let data = [
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Logan',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee'
+		},
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Sean',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee'
+		},
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Evan',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee'
+		},
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Don',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee'
+		},
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Mark',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee',
+			HealthScore: '2'
+		},
+		{
+			number: '001',
+			pic: '',
+			first_name: 'Maxine',
+			last_name: 'Nguyen',
+			DOB: '9/15/98',
+			Age: '24',
+			Sex: 'M',
+			Gender: 'M',
+			Address: '222 Main St. Detroit',
+			'Phone Number': '555-555-5555',
+			Email: 'lg@asd.com',
+			'Last Seen': '5/5/23',
+			PCP: 'Dr. Geedee',
+			HealthScore: '2'
+		}
+	];
 </script>
-<TabList bind:this={comp}></TabList>
-<div class="">
-    <table class="overflow-hidden absolute left-20 top-20 mx-5 my-8 rounded-lg shadow w-[92%]">
-        <thead class="bg-gray-50 border-b-2 border-gray-200">
-            <tr>			
 
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">#</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">pic</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">First Name</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Last Name</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">DOB</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Age</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Sex</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Gender</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Address</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Phone Number</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Email</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Last Seen</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Physician</th>
-                <th class="p-3 text-sm font-semibold tracking-wide text-left">Health Score</th>
-            </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-100">
-            {#each Object.values(data) as row}
-                <tr on:click="{() => {comp.addTab()}}" class="bg-white">
-                    {#each Object.values(row) as cell}
-                        <td class="px-3 py-5 text-sm text-left text-gray-700 whitespace-nowrap">{cell}</td>
-                    {/each}
-                </tr>
-            {/each}                
-        </tbody>
-    </table>
+<TabList bind:this={comp} />
+<div class="">
+	<table class="overflow-hidden absolute left-20 top-20 mx-5 my-8 rounded-lg shadow w-[92%]">
+		<thead class="bg-gray-50 border-b-2 border-gray-200">
+			<tr>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">#</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">pic</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">First Name</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Last Name</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">DOB</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Age</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Sex</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Gender</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Address</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Phone Number</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Email</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Last Seen</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Physician</th>
+				<th class="p-3 text-sm font-semibold tracking-wide text-left">Health Score</th>
+			</tr>
+		</thead>
+		<tbody class="divide-y divide-gray-100">
+			{#each data as row}
+				<tr
+					on:click={() => goto(`/patient/${row.number}`)}
+					class="bg-white cursor-pointer hover:bg-gray-100 transition-colors"
+				>
+					{#each Object.values(row) as cell}
+						<td class="px-3 py-5 text-sm text-left text-gray-700 whitespace-nowrap"
+							>{cell || '--'}
+						</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
